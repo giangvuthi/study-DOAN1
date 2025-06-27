5b6ed1f26913645c038d332ed8876faf30294124
giangvuthi <vtgiang.dhti16a5hn@sv.uneti.vn>
2025-06-27 07:58:37 +0700

    first commit

--- DOAN1/Form1.cs
+++ DOAN1/Form1.cs
@@ -6,5 +6,10 @@ namespace DOAN1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // first commit 
+        }
     }
 }
