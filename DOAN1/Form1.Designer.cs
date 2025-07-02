4e71be3e21e1f91214bad0597259cba2a937054e
anhLinh <lethianhlinh2005@gmail.com>
2025-07-02 15:36:15 +0700

    formhoadon

--- DOAN1/Form1.Designer.cs
+++ DOAN1/Form1.Designer.cs
@@ -28,9 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnChiTietHoaDon = new Button();
             label1 = new Label();
-            dgvHoaDon = new DataGridView();
             toolStrip1 = new ToolStrip();
             tsThem = new ToolStripButton();
             tsSua = new ToolStripButton();
@@ -38,45 +36,38 @@
             tsXoa = new ToolStripButton();
             tsChitiet = new ToolStripButton();
             tslammoi = new ToolStripButton();
-            panelChiTiet = new Panel();
-            dgvChiTietHoaDon = new DataGridView();
-            label2 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dgvHoaDon).BeginInit();
+            txtMahd = new TextBox();
+            cbMakh = new ComboBox();
+            dtpNgaylap = new DateTimePicker();
+            txtManv = new TextBox();
+            lblTongThanhTien = new Label();
+            grbThongtinchung = new GroupBox();
+            groupBox1 = new GroupBox();
+            btnThemsp = new Button();
+            nudSoluong = new NumericUpDown();
+            txtTonkho = new TextBox();
+            txtDongiaban = new TextBox();
+            this.txtTensp = new TextBox();
+            cbSanpham = new ComboBox();
+            dgvChitiethoadon = new DataGridView();
+            btnTaohoadon = new Button();
             toolStrip1.SuspendLayout();
-            panelChiTiet.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvChiTietHoaDon).BeginInit();
+            grbThongtinchung.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)nudSoluong).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvChitiethoadon).BeginInit();
             SuspendLayout();
-            // 
-            // btnChiTietHoaDon
-            // 
-            btnChiTietHoaDon.Location = new Point(1232, 478);
-            btnChiTietHoaDon.Name = "btnChiTietHoaDon";
-            btnChiTietHoaDon.Size = new Size(94, 29);
-            btnChiTietHoaDon.TabIndex = 2;
-            btnChiTietHoaDon.Text = "Chi Tiết Hóa Đơn";
-            btnChiTietHoaDon.UseVisualStyleBackColor = true;
-            btnChiTietHoaDon.Click += btnChiTietHoaDon_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
-            label1.Location = new Point(690, 27);
+            label1.Location = new Point(831, 89);
+            label1.Margin = new Padding(5, 0, 5, 0);
             label1.Name = "label1";
-            label1.Size = new Size(104, 25);
+            label1.Size = new Size(333, 40);
             label1.TabIndex = 10;
-            label1.Text = "HÓA ĐƠN ";
-            // 
-            // dgvHoaDon
-            // 
-            dgvHoaDon.AllowUserToResizeRows = false;
-            dgvHoaDon.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvHoaDon.Location = new Point(0, 67);
-            dgvHoaDon.Name = "dgvHoaDon";
-            dgvHoaDon.RowHeadersWidth = 51;
-            dgvHoaDon.Size = new Size(1384, 400);
-            dgvHoaDon.TabIndex = 11;
-            dgvHoaDon.CellContentClick += dataGridView1_CellContentClick;
+            label1.Text = "THÔNG TIN HOA DON ";
             // 
             // toolStrip1
             // 
@@ -84,7 +75,8 @@
             toolStrip1.Items.AddRange(new ToolStripItem[] { tsThem, tsSua, toolStripSeparator1, tsXoa, tsChitiet, tslammoi });
             toolStrip1.Location = new Point(0, 0);
             toolStrip1.Name = "toolStrip1";
-            toolStrip1.Size = new Size(1384, 27);
+            toolStrip1.Padding = new Padding(0, 0, 3, 0);
+            toolStrip1.Size = new Size(2040, 42);
             toolStrip1.TabIndex = 12;
             toolStrip1.Text = "toolStrip1";
             // 
@@ -93,7 +85,7 @@
             tsThem.Image = Properties.Resources.icons8_add_48;
             tsThem.ImageTransparentColor = Color.Magenta;
             tsThem.Name = "tsThem";
-            tsThem.Size = new Size(70, 24);
+            tsThem.Size = new Size(99, 36);
             tsThem.Text = "Thêm";
             tsThem.Click += tsThem_Click;
             // 
@@ -102,20 +94,20 @@
             tsSua.Image = Properties.Resources.icons8_update_48;
             tsSua.ImageTransparentColor = Color.Magenta;
             tsSua.Name = "tsSua";
-            tsSua.Size = new Size(58, 24);
+            tsSua.Size = new Size(77, 36);
             tsSua.Text = "Sửa";
             // 
             // toolStripSeparator1
             // 
             toolStripSeparator1.Name = "toolStripSeparator1";
-            toolStripSeparator1.Size = new Size(6, 27);
+            toolStripSeparator1.Size = new Size(6, 42);
             // 
             // tsXoa
             // 
             tsXoa.Image = Properties.Resources.icons8_delete_48;
             tsXoa.ImageTransparentColor = Color.Magenta;
             tsXoa.Name = "tsXoa";
-            tsXoa.Size = new Size(59, 24);
+            tsXoa.Size = new Size(78, 36);
             tsXoa.Text = "Xóa";
             // 
             // tsChitiet
@@ -123,7 +115,7 @@
             tsChitiet.Image = Properties.Resources.icons8_detail_48;
             tsChitiet.ImageTransparentColor = Color.Magenta;
             tsChitiet.Name = "tsChitiet";
-            tsChitiet.Size = new Size(112, 24);
+            tsChitiet.Size = new Size(166, 36);
             tsChitiet.Text = "Xem chi tiết";
             tsChitiet.Click += tsChitiet_Click;
             // 
@@ -132,65 +124,177 @@
             tslammoi.Image = Properties.Resources.icons8_refresh;
             tslammoi.ImageTransparentColor = Color.Magenta;
             tslammoi.Name = "tslammoi";
-            tslammoi.Size = new Size(91, 24);
+            tslammoi.Size = new Size(130, 36);
             tslammoi.Text = "Làm mới";
             // 
-            // panelChiTiet
+            // txtMahd
             // 
-            panelChiTiet.Controls.Add(dgvChiTietHoaDon);
-            panelChiTiet.Controls.Add(label2);
-            panelChiTiet.Location = new Point(0, 513);
-            panelChiTiet.Name = "panelChiTiet";
-            panelChiTiet.Size = new Size(1372, 364);
-            panelChiTiet.TabIndex = 13;
-            panelChiTiet.Visible = false;
+            txtMahd.Location = new Point(24, 56);
+            txtMahd.Name = "txtMahd";
+            txtMahd.PlaceholderText = "Ma hoa don ";
+            txtMahd.ReadOnly = true;
+            txtMahd.Size = new Size(170, 39);
+            txtMahd.TabIndex = 13;
+            txtMahd.TextChanged += textBox1_TextChanged;
             // 
-            // dgvChiTietHoaDon
+            // cbMakh
             // 
-            dgvChiTietHoaDon.AllowUserToResizeRows = false;
-            dgvChiTietHoaDon.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvChiTietHoaDon.Location = new Point(0, 68);
-            dgvChiTietHoaDon.Name = "dgvChiTietHoaDon";
-            dgvChiTietHoaDon.RowHeadersWidth = 51;
-            dgvChiTietHoaDon.Size = new Size(1384, 300);
-            dgvChiTietHoaDon.TabIndex = 14;
+            cbMakh.FormattingEnabled = true;
+            cbMakh.Location = new Point(532, 56);
+            cbMakh.Name = "cbMakh";
+            cbMakh.Size = new Size(170, 40);
+            cbMakh.TabIndex = 14;
             // 
-            // label2
+            // dtpNgaylap
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold);
-            label2.Location = new Point(608, 20);
-            label2.Name = "label2";
-            label2.Size = new Size(178, 25);
-            label2.TabIndex = 11;
-            label2.Text = "CHI TIẾT HÓA ĐƠN";
+            dtpNgaylap.Location = new Point(800, 57);
+            dtpNgaylap.Name = "dtpNgaylap";
+            dtpNgaylap.Size = new Size(370, 39);
+            dtpNgaylap.TabIndex = 15;
+            // 
+            // txtManv
+            // 
+            txtManv.Location = new Point(286, 56);
+            txtManv.Name = "txtManv";
+            txtManv.PlaceholderText = "Ma nhan vien";
+            txtManv.Size = new Size(170, 39);
+            txtManv.TabIndex = 16;
+            // 
+            // lblTongThanhTien
+            // 
+            lblTongThanhTien.Location = new Point(1264, 56);
+            lblTongThanhTien.Name = "lblTongThanhTien";
+            lblTongThanhTien.Size = new Size(378, 40);
+            lblTongThanhTien.TabIndex = 17;
+            lblTongThanhTien.Text = "l";
+            // 
+            // grbThongtinchung
+            // 
+            grbThongtinchung.Controls.Add(dtpNgaylap);
+            grbThongtinchung.Controls.Add(lblTongThanhTien);
+            grbThongtinchung.Controls.Add(txtMahd);
+            grbThongtinchung.Controls.Add(txtManv);
+            grbThongtinchung.Controls.Add(cbMakh);
+            grbThongtinchung.Location = new Point(115, 146);
+            grbThongtinchung.Name = "grbThongtinchung";
+            grbThongtinchung.Size = new Size(1720, 122);
+            grbThongtinchung.TabIndex = 18;
+            grbThongtinchung.TabStop = false;
+            grbThongtinchung.Text = "groupBox1";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnThemsp);
+            groupBox1.Controls.Add(nudSoluong);
+            groupBox1.Controls.Add(txtTonkho);
+            groupBox1.Controls.Add(txtDongiaban);
+            groupBox1.Controls.Add(this.txtTensp);
+            groupBox1.Controls.Add(cbSanpham);
+            groupBox1.Location = new Point(115, 284);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1720, 122);
+            groupBox1.TabIndex = 19;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // btnThemsp
+            // 
+            btnThemsp.Location = new Point(1281, 38);
+            btnThemsp.Name = "btnThemsp";
+            btnThemsp.Size = new Size(150, 46);
+            btnThemsp.TabIndex = 21;
+            btnThemsp.Text = "Them san pham";
+            btnThemsp.UseVisualStyleBackColor = true;
+            // 
+            // nudSoluong
+            // 
+            nudSoluong.Location = new Point(1037, 53);
+            nudSoluong.Name = "nudSoluong";
+            nudSoluong.Size = new Size(133, 39);
+            nudSoluong.TabIndex = 20;
+            // 
+            // txtTonkho
+            // 
+            txtTonkho.Location = new Point(800, 53);
+            txtTonkho.Name = "txtTonkho";
+            txtTonkho.PlaceholderText = "So luong con trong kho ";
+            txtTonkho.ReadOnly = true;
+            txtTonkho.Size = new Size(170, 39);
+            txtTonkho.TabIndex = 19;
+            // 
+            // txtDongiaban
+            // 
+            txtDongiaban.Location = new Point(532, 53);
+            txtDongiaban.Name = "txtDongiaban";
+            txtDongiaban.PlaceholderText = "Don gia";
+            txtDongiaban.ReadOnly = true;
+            txtDongiaban.Size = new Size(170, 39);
+            txtDongiaban.TabIndex = 18;
+            // 
+            // txtTensp
+            // 
+            this.txtTensp.Location = new Point(286, 56);
+            this.txtTensp.Name = "txtTensp";
+            this.txtTensp.PlaceholderText = "Ten san pham";
+            this.txtTensp.ReadOnly = true;
+            this.txtTensp.Size = new Size(170, 39);
+            this.txtTensp.TabIndex = 16;
+            // 
+            // cbSanpham
+            // 
+            cbSanpham.FormattingEnabled = true;
+            cbSanpham.Location = new Point(24, 59);
+            cbSanpham.Name = "cbSanpham";
+            cbSanpham.Size = new Size(178, 40);
+            cbSanpham.TabIndex = 14;
+            // 
+            // dgvChitiethoadon
+            // 
+            dgvChitiethoadon.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvChitiethoadon.Location = new Point(115, 439);
+            dgvChitiethoadon.Name = "dgvChitiethoadon";
+            dgvChitiethoadon.RowHeadersWidth = 82;
+            dgvChitiethoadon.Size = new Size(1730, 152);
+            dgvChitiethoadon.TabIndex = 22;
+            // 
+            // btnTaohoadon
+            // 
+            btnTaohoadon.Location = new Point(667, 698);
+            btnTaohoadon.Name = "btnTaohoadon";
+            btnTaohoadon.Size = new Size(150, 46);
+            btnTaohoadon.TabIndex = 22;
+            btnTaohoadon.Text = "Tao hoa don";
+            btnTaohoadon.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1384, 877);
-            Controls.Add(panelChiTiet);
+            ClientSize = new Size(2040, 1025);
+            Controls.Add(btnTaohoadon);
+            Controls.Add(dgvChitiethoadon);
+            Controls.Add(groupBox1);
+            Controls.Add(grbThongtinchung);
             Controls.Add(toolStrip1);
             Controls.Add(label1);
-            Controls.Add(btnChiTietHoaDon);
-            Controls.Add(dgvHoaDon);
+            Margin = new Padding(5);
             Name = "Form1";
+            Text = "--";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)dgvHoaDon).EndInit();
             toolStrip1.ResumeLayout(false);
             toolStrip1.PerformLayout();
-            panelChiTiet.ResumeLayout(false);
-            panelChiTiet.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvChiTietHoaDon).EndInit();
+            grbThongtinchung.ResumeLayout(false);
+            grbThongtinchung.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)nudSoluong).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvChitiethoadon).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private Button btnChiTietHoaDon;
         private Label label1;
-        private DataGridView dgvHoaDon;
         private ToolStrip toolStrip1;
         private ToolStripButton tsThem;
         private ToolStripButton tsSua;
@@ -198,8 +302,23 @@
         private ToolStripButton tsXoa;
         private ToolStripButton tsChitiet;
         private ToolStripButton tslammoi;
-        private Panel panelChiTiet;
+        private TextBox txtMahd;
+        private ComboBox cbMakh;
+        private DateTimePicker dtpNgaylap;
+        private TextBox txtManv;
+        private Label lblTongThanhTien;
+        private GroupBox grbThongtinchung;
+        private GroupBox groupBox1;
+        private DateTimePicker dateTimePicker1;
         private Label label2;
-        private DataGridView dgvChiTietHoaDon;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox cbSanpham;
+        private Button btnThemsp;
+        private NumericUpDown nudSoluong;
+        private TextBox txtTonkho;
+        private TextBox txtDongiaban;
+        private DataGridView dgvChitiethoadon;
+        private Button btnTaohoadon;
     }
 }
--- DOAN1/Form1.cs
+++ DOAN1/Form1.cs
@@ -23,50 +23,12 @@ namespace DOAN1
         }
         private void LoadHoaDon()
         {
-            try
-            {
-                conn.Open();
-                string query = "SELECT * FROM hoadon"; // hoặc truy vấn cụ thể hơn
-
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-
-                adapter.Fill(dt);
-                dgvHoaDon.DataSource = dt;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
-            }
-            finally
-            {
-                conn.Close();
-            }
+            
         }
 
         private void LoadChiTietHoaDon(string maHD)
         {
-            try
-            {
-                conn.Open();
-                string query = "SELECT maHoaDon,maSanPham, soLuong, donGiaBan, thanhTien FROM  tt_chitiet_hoadon WHERE maHoaDon = @maHoaDon";
-                MySqlCommand cmd = new MySqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@maHoaDon", maHD);
-
-                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                dgvChiTietHoaDon.DataSource = dt;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Lỗi chi tiết: " + ex.Message);
-            }
-            finally
-            {
-                conn.Close();
-            }
+          
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -76,45 +38,22 @@ namespace DOAN1
 
         private void btnChiTietHoaDon_Click(object sender, EventArgs e)
         {
-           
+
         }
-        enum TrangDangXem { HoaDon, ChiTiet }
-        TrangDangXem trangHienTai = TrangDangXem.HoaDon;
+        
 
         private void tsThem_Click(object sender, EventArgs e)
         {
-            if (trangHienTai == TrangDangXem.HoaDon)
-            {
-                // Thêm hóa đơn mới
-                FormThemHoaDon f = new FormThemHoaDon();
-                if (f.ShowDialog() == DialogResult.OK)
-                {
-                    LoadHoaDon(); // Load lại
-                }
-            }
-            else if (trangHienTai == TrangDangXem.ChiTiet)
-            {
-                if (dgvHoaDon.CurrentRow != null)
-                {
-                    string maHD = dgvHoaDon.CurrentRow.Cells["maHoaDon"].Value.ToString();
-                    FormThemChiTiet f = new FormThemChiTiet(maHD); // Truyền mã hóa đơn để thêm đúng
-                    if (f.ShowDialog() == DialogResult.OK)
-                    {
-                        LoadChiTietHoaDon(maHD);
-                    }
-                }
-            }
+            
         }
 
         private void tsChitiet_Click(object sender, EventArgs e)
         {
-            if (dgvHoaDon.CurrentRow != null)
-            {
-                string maHD = dgvHoaDon.CurrentRow.Cells["maHoaDon"].Value.ToString();
-                LoadChiTietHoaDon(maHD);
-                panelChiTiet.Visible = true;
-                trangHienTai = TrangDangXem.ChiTiet; // Chuyển trạng thái
-            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            txtMahd.Text = "";
         }
     }
 }
